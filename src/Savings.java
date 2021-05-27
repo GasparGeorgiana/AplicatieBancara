109f274fda625c614fa11d00a6c3d9d985d3ff1c
GasparGeorgiana <73753872+GasparGeorgiana@users.noreply.github.com>
2021-05-27 16:38:10 +0300

    Update Savings.java

--- src/Savings.java
+++ src/Savings.java
@@ -1,18 +1,36 @@
-public class Savings extends Cont {
-    private static String tipulContului = "Savings";
+package Clase;
 
-    Savings(double depunereInitiala) {
-        super();
-        this.setBalanta(depunereInitiala);
-        this.verificaDobanda(0);
+import Clase.Cont;
+
+public class Savings extends Cont {
+    private int savings_id;
+    private static String tipulContului = "Clase.Savings";
+
+    public Savings(int cont_id, double balanta, double dobanda, int numarCont, int savings_id) {
+        super(cont_id, balanta, dobanda, numarCont);
+        this.savings_id = savings_id;
+    }
+
+    public int getSavings_id() {
+        return savings_id;
+    }
+
+    public void setSavings_id(int savings_id) {
+        this.savings_id = savings_id;
+    }
+
+    public static String getTipulContului() {
+        return tipulContului;
+    }
+
+    public static void setTipulContului(String tipulContului) {
+        Savings.tipulContului = tipulContului;
     }
 
     @Override
     public String toString() {
-        return "Tipul Contului: " + tipulContului + "Cont\n" +
-                "Numarul Contului: " + this.getNumarCont() + "\n" +
-                "Balanta: " + this.getBalanta() + "\n" +
-                "Rata dobanzii: " + this.getDobanda() + "%\n";
-
+        return "Savings{" +
+                "savings_id=" + savings_id +
+                '}';
     }
 }
